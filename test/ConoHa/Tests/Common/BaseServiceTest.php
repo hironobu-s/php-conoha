7f3fe9e350fd0a011bdd84dec2e40cc2b0536ad7
Hironobu <hiro@hironobu.org>
2015-07-06 21:39:12 +0900

    initial commit.

---
+++ test/ConoHa/Tests/Common/BaseServiceTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace ConoHa\Tests\Common;
+
+use ConoHa\Common\BaseService;
+use ConoHa\Identity\Resource\Token;
+
+class TestService extends BaseService
+{
+    protected $properties = [
+        'prop1' => null,
+        'prop_test2' => null,
+        'prop_test_no3' => null,
+    ];
+}
+
+class BaseServiceTest extends \PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        $this->service = new TestService(TEST_IDENTITY_ENDPOINT);
+    }
+
+    public function testUri()
+    {
+        $this->service->setEndpoint(TEST_IDENTITY_ENDPOINT);
+
+        $uri = $this->service->getUri('foo');
+        $this->assertEquals($uri, TEST_IDENTITY_ENDPOINT . '/foo');
+
+        $uri = $this->service->getUri(['foo', 'bar', 'baz']);
+        $this->assertEquals($uri, TEST_IDENTITY_ENDPOINT . '/foo/bar/baz');
+    }
+
+    public function testSetToken()
+    {
+        $token = new Token();
+        $this->service->setToken($token);
+    }
+
+    public function testGetVersions()
+    {
+        if(!API_TEST) {
+            return ;
+        }
+
+        $v = $this->service->getVersions();
+        $this->assertInstanceOf('ConoHa\Common\Resource\Versions', $v);
+    }
+
+    public function testGetVersion()
+    {
+        if(!API_TEST) {
+            return ;
+        }
+
+        $v = $this->service->getVersion("v2.0");
+        $this->assertInstanceOf('ConoHa\Common\Resource\Version', $v);
+    }
+}
---
+++ test/ConoHa/Tests/Common/ObjctTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace ConoHa\Tests\Common;
+
+use ConoHa\Common\Object;
+
+class TestObject extends Object
+{
+    protected $properties = [
+        'prop1' => null,
+        'prop_test2' => null,
+        'prop_test_no3' => null,
+    ];
+}
+
+class ObjectTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSetter()
+    {
+        $obj = new TestObject();
+        $obj->setProp1("");
+        $obj->setPropTest2("");
+        $obj->setPropTestNo3("");
+    }
+
+    public function testGetter()
+    {
+        $obj = new TestObject();
+        $obj->getProp1();
+        $obj->getPropTest2();
+        $obj->getPropTestNo3();
+    }
+
+    /**
+     * @expectedException BadMethodCallException
+     */
+    public function testUndefinededWrite() {
+        $obj = new TestObject();
+        $obj->setUndefinedMethod("value");
+    }
+
+    /**
+     * @expectedException BadMethodCallException
+     */
+    public function testUndefinededRead() {
+        $obj = new TestObject();
+        $obj->getUndefinedMethod();
+    }
+
+    public function testSnake2Camel()
+    {
+        $obj = new TestObject();
+
+        $before = 'foo_bar_baz';
+        $after  = 'FooBarBaz';
+        $result = $obj->snake2Camel($before);
+        $this->assertEquals($result, $after);
+    }
+
+    public function testCamel2Snake()
+    {
+        $obj = new TestObject();
+
+        $before  = 'FooBarBaz';
+        $after = 'foo_bar_baz';
+        $result = $obj->camel2Snake($before);
+        $this->assertEquals($result, $after);
+    }
+}
