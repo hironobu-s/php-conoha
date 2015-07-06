a3f566473320e04f7d44ebe850d86de22311ee53
Hironobu <hiro@hironobu.org>
2015-07-06 16:10:29 +0900

    initial commit.

---
+++ test/ConoHa/Tests/Identity/Resource/TokenTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace ConoHa\Tests\Identity\Resource;
+
+use ConoHa\Identity\Resource\Token;
+
+class TokenTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSetter() {
+        $s = new Token();
+        $s->setId("value");
+        $s->setExpires("2015-01-01");
+        $s->setIssuedAt("value");
+    }
+
+    public function testGetter() {
+        $s = new Token();
+        $s->getId();
+        $s->getExpires();
+        $s->getIssuedAt();
+    }
+
+    public function testExpires() {
+        $date = "2015-07-01 12:00:00";
+
+        $s = new Token();
+        $s->setExpires($date);
+        $expires = $s->getExpires();
+
+        $d = new \DateTime($date);
+        $this->assertEquals($d->diff($expires)->format('%s'), 0);
+    }
+
+    /**
+     * @expectedException BadMethodCallException
+     */
+    public function testUndefinededWrite() {
+        $s = new Token();
+        $s->setUndefinedMethod("value");
+    }
+
+    /**
+     * @expectedException BadMethodCallException
+     */
+    public function testUndefinededRead() {
+        $s = new Token();
+        $v = $s->getUndefinedMethod();
+    }
+}
