f83e74c3e4b415c19a17cb2b23afb28d8707fe50
Hironobu <hiro@hironobu.org>
2015-07-08 23:36:42 +0900

    Endpoint URLをSecretに持たせるように変更

--- src/ConoHa/ConoHa.php
+++ src/ConoHa/ConoHa.php
@@ -11,25 +11,33 @@ class ConoHa extends Object
 {
     private $access;
     private $identity_endpoint;
-    public function __construct($identity_endpoint)
+    public function __construct()
     {
-        $this->identity_endpoint = $identity_endpoint;
     }
 
     public function auth(Secret $secret)
     {
-        $ident = $this->getIdentityService();
+        $endpoint = $secret->getAuthUrl();
+
+        $ident = $this->getIdentityService($endpoint);
         $this->access = $ident->tokens($secret);
 
         return $this->access;
     }
 
-    public function getIdentityService()
+    public function getIdentityService($endpoint = null)
     {
-        $s = new IdentityService($this->identity_endpoint);
         if($this->access instanceof Access) {
+            $s = new IdentityService($endpoint);
             $s->setToken($this->access->getToken());
+
+        } else if($endpoint != null) {
+            $s = new IdentityService($endpoint);
+
+        } else {
+            throw new InvalidArgumentException('The endpoint url is required if auth() method does not called.');
         }
+
         return $s;
     }
 }
