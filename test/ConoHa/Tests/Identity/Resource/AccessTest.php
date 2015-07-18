fa70aab7f30324693cb011d4ff86a70a982978c9
Hironobu <hiro@hironobu.org>
2015-07-18 19:53:14 +0900

    テスト追加

--- test/ConoHa/Tests/Identity/Resource/AccessTest.php
+++ test/ConoHa/Tests/Identity/Resource/AccessTest.php
@@ -3,10 +3,14 @@
 namespace ConoHa\Tests\Identity\Resource;
 
 use ConoHa\ConoHa;
+use ConoHa\Api\Client;
+use ConoHa\Identity\Resource\Token;
+use ConoHa\Identity\Resource\ServiceCatalog;
 use ConoHa\Identity\Resource\Access;
 
 class AccessTest extends \PHPUnit_Framework_TestCase
 {
+    protected static $conoha;
     protected static $access;
     public static function setUpBeforeClass()
     {
@@ -17,11 +21,33 @@ class AccessTest extends \PHPUnit_Framework_TestCase
 
     public function testGetToken()
     {
-        $this->assertInstanceOf('\ConoHa\Identity\Resource\Token', self::$access->getToken());
+        $this->assertInstanceOf('ConoHa\Identity\Resource\Token', self::$access->getToken());
     }
 
     public function testGetServiceCatalog()
     {
-        $this->assertInstanceOf('\ConoHa\Identity\Resource\ServiceCatalog', self::$access->getServiceCatalog());
+        $this->assertInstanceOf('ConoHa\Identity\Resource\ServiceCatalog', self::$access->getServiceCatalog());
+    }
+
+    public function testSetToken()
+    {
+        $token = new Token();
+        $ret = self::$access->setToken($token);
+        $this->assertInstanceOf('ConoHa\Identity\Resource\Token', $ret);
+    }
+
+    public function testSetServiceCatalog()
+    {
+        $sc = new ServiceCatalog();
+        $ret = self::$access->setServiceCatalog($sc);
+        $this->assertInstanceOf('ConoHa\Identity\Resource\ServiceCatalog', $ret);
+    }
+
+    public function testPopulate()
+    {
+        $conoha = new ConoHa();
+        $ident = $conoha->getIdentityService(TEST_IDENTITY_ENDPOINT);
+        $secret = __get_test_secret();
+        $ident->tokens($secret);
     }
 }
