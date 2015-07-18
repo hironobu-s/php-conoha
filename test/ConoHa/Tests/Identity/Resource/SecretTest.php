6fc75908000221d321184706ebf1c68ce9a70b39
Hironobu <hiro@hironobu.org>
2015-07-18 19:47:35 +0900

    AuthURLに関するテストを追加。

--- test/ConoHa/Tests/Identity/Resource/SecretTest.php
+++ test/ConoHa/Tests/Identity/Resource/SecretTest.php
@@ -8,10 +8,28 @@ class SecretTest extends \PHPUnit_Framework_TestCase
 {
     public function testSetter() {
         $s = new Secret();
-        $s->setUsername("value");
-        $s->setPassword("value");
-        $s->setTenantName("value");
-        $s->setTenantId("value");
+        $s->setUsername(TEST_API_USERNAME);
+        $s->setPassword(TEST_API_PASSWORD);
+        $s->setTenantName(TEST_API_TENANT_NAME);
+        $s->setTenantId(TEST_API_TENANT_ID);
+        $s->setAuthUrl(TEST_IDENTITY_ENDPOINT);
+    }
+
+    public function testNoSchemeUrl() {
+        $info = parse_url(TEST_IDENTITY_ENDPOINT);
+        $s = new Secret();
+        $s->setAuthUrl("//" . $info['host']);
+
+        $url = $s->getAuthUrl();
+        $this->assertEquals(TEST_IDENTITY_ENDPOINT, $url);
+    }
+
+    /**
+     * @expectedException ConoHa\Exception\IncorrectUrlException
+     */
+    public function testInvalidUrl() {
+        $s = new Secret();
+        $s->setAuthUrl("invalid url");
     }
 
     public function testGetter() {
@@ -20,5 +38,6 @@ class SecretTest extends \PHPUnit_Framework_TestCase
         $s->getPassword();
         $s->getTenantName();
         $s->getTenantId();
+        $s->getAuthUrl();
     }
 }
