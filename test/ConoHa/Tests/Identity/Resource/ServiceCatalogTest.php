9857aa87bed77553b5a690a8e82282e54ea834ab
Hironobu <hiro@hironobu.org>
2015-07-21 16:52:48 +0900

    SetterとGetterについてassertされていなかったのを修正

--- test/ConoHa/Tests/Identity/Resource/ServiceCatalogTest.php
+++ test/ConoHa/Tests/Identity/Resource/ServiceCatalogTest.php
@@ -8,17 +8,14 @@ class ServiceCatalogTest extends \PHPUnit_Framework_TestCase
 {
     public function testSetter() {
         $s = new ServiceCatalog();
-        $s->setEndpoints([]);
-        $s->setEndpointLinks([]);
-        $s->setName('');
-        $s->setType('');
-    }
+        $s->setEndpoints(['endpoint']);
+        $s->setEndpointLinks(['endpoint-links']);
+        $s->setName('test-name');
+        $s->setType('test-type');
 
-    public function testGetter() {
-        $s = new ServiceCatalog();
-        $s->getEndpoints();
-        $s->getEndpointLinks();
-        $s->getName();
-        $s->getType();
+        $this->assertEquals(['endpoint'], $s->getEndpoints());
+        $this->assertEquals(['endpoint-links'], $s->getEndpointLinks());
+        $this->assertEquals('test-name', $s->getName());
+        $this->assertEquals('test-type', $s->getType());
     }
 }
