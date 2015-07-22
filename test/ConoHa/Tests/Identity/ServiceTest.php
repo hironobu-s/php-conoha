848f86eab2c3ef61cd295c37b5ec172ea4baeab8
Hironobu <hiro@hironobu.org>
2015-07-22 17:42:02 +0900

    テストを追加

--- test/ConoHa/Tests/Identity/ServiceTest.php
+++ test/ConoHa/Tests/Identity/ServiceTest.php
@@ -13,6 +13,25 @@ class ServiceTest extends \PHPUnit_Framework_TestCase
         $service = new Service(TEST_IDENTITY_ENDPOINT);
         $access = $service->tokens($secret);
         $this->assertInstanceOf('ConoHa\Identity\Resource\Access', $access);
+
+        $token = $access->getToken();
+        $this->assertInstanceOf('ConoHa\Identity\Resource\Token', $token);
+        $this->assertNotNull($token->getId());
+
+        $catalog = $access->getServiceCatalog();
+        $this->assertInstanceOf('ConoHa\Common\ResourceCollection', $catalog);
+        $this->assertGreaterThan(1, count($catalog));
+
+        $this->assertNotNull($access->getCatalogByType('identity'));
+        $this->assertNotNull($access->getCatalogByType('compute'));
+        $this->assertNotNull($access->getCatalogByType('image'));
+        $this->assertNotNull($access->getCatalogByType('volumev2'));
+        $this->assertNotNull($access->getCatalogByType('network'));
+        $this->assertNotNull($access->getCatalogByType('object-store'));
+        $this->assertNotNull($access->getCatalogByType('dns'));
+        $this->assertNotNull($access->getCatalogByType('mailhosting'));
+        $this->assertNotNull($access->getCatalogByType('databasehosting'));
+        $this->assertNotNull($access->getCatalogByType('account'));
     }
 
     public function testTokenFail()
